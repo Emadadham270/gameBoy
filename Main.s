72c277a3f962fd8f0d0af3ee442b3568d22685a4
Mathieu Morcos <mathieubotros@gmail.com>
2025-04-27 00:20:34 +0300

    Cleaner main

--- Main.s
+++ Main.s
@@ -1,5 +1,5 @@
 	AREA MYDATAS, DATA, READONLY
-	;--- Colors ---
+;--- Colors ---
 Red     EQU 0Xf800 
 Green   EQU 0xF0FF
 Blue    EQU 0x02ff 
@@ -30,23 +30,12 @@ GPIO_ODR        EQU     0x14
 	IMPORT TFT_Filldraw4INP
 	IMPORT GET_state
 	IMPORT delay
-	IMPORT Draw_XO
-	IMPORT Check_Win	
-	IMPORT Update_Left_Sidebar
-	IMPORT TFT_MoveCursor 
-	IMPORT X1
-	IMPORT O1
+
 	IMPORT Main_Game_XO
-	IMPORT DrawBorder
-X_start			DCB		0X70
-X_end			DCB		0XD2
-Y_start			DCB		0X70
-Y_END			DCB		0XD2
 
 __main FUNCTION
 
-
-	;FINAL TODO: CALL FUNCTION SETUP
+	;CALL FUNCTION SETUP
 	;BL SETUP ; all THE INTIALIZATION
 	LDR R0, =RCC_BASE + RCC_AHB1ENR
     LDR R1, [R0]
@@ -76,44 +65,15 @@ __main FUNCTION
  	LDR R0, =GPIOB_BASE + GPIO_OTYPER
  	MOV R2, #0x00000000
  	STR R2, [R0]
-	 	;PULL UP 
+	;PULL UP 
  	LDR R0, =GPIOB_BASE + GPIO_PUPDR
  	MOV R2, #0x55555555
  	STR R2, [R0]
     ; Initialize TFT
     BL TFT_Init
-    ; Fill screen with color
-	;MOV R1, #0X02        ; Start X
-    ;MOV R2, #0X70         ; Start Y
-	;LDR R3, =X1		    ; Load image address
-    ;BL TFT_DrawImage
+
 	BL Main_Game_XO
-	;MOV R0,;THENUM OF THE PHOTO 
-	;MOV R5,#NUMX
-	;MOV R6, #NUMY
-	;BL ;DRAW PHOTO
-	;MOV R10,#0
-    ; Fill screen with color (line)
-    ;MOV R0, #Black
-	;MOV R11,#Yellow
-;INNERLOOP
-	;MOV R1,#0X70
-	;MOV R2,#0X70
-;IN2
-	;BL GET_state
-	;AND R10,R10,#0X001F 
-	;CMP R10,#0
-	;BEQ IN2
-	;CMP R10,#16
-	;BEQ ENTER5
-	;BL TFT_MoveCursor 
-	;BL IN2
-	
-	
-;ENTER5
-	;LDR R3,=X1
-	;BL TFT_DrawImage
-	;BL INNERLOOP
+
     ENDFUNC
 	
 	END
